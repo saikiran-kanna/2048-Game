12233920c991489f8b23c053deea792f63245253
Sai Kiran Bompelliwar <118735380+saikiran-kanna@users.noreply.github.com>
2024-04-06 03:08:52 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,23 +2,19 @@ function createBoard(){
     const grid = document.querySelector(".grid");
     for(let i = 0; i < 16; i++){
         const divi = document.createElement("div");
-        divi.innerText = 0;
+        divi.innerText = " ";
         divi.id = `${i}`;
         grid.append(divi);
     }
 }
 function generate(){
     let numArr = [2,2,2,2,2,2,2,2,2,4];
-    let ind = Math.floor(Math.random()*10);
-    const num = numArr[ind];
-    let all_blocks = Array.from(document.querySelector('.grid').children);
-    let filteredBlocks = all_blocks.filter((item)=> item.textContent == 0);
-    if(filteredBlocks.length == 0) return false;
+    const num = numArr[Math.floor(Math.random()*10)];
+    let filteredBlocks = Array.from(document.querySelector('.grid').children).filter((item)=> item.textContent === " ");
+    if(filteredBlocks.length == 0) return;
     let block = filteredBlocks[Math.floor(Math.random()*filteredBlocks.length)];
     block.innerText = num;
-    block.style.visibility = "visible";
-    return true;
-}
+} 
 function leftshift(arr){
     let filtered_arr = arr.filter(it => it != 0);
     for(let i = filtered_arr.length; i < 4; i++) filtered_arr.push(0);
@@ -37,29 +33,46 @@ function rowOperation(cb){
     for(let i = 0; i < 4; i++){
         let new_arr = [];
         for(let j = s; j < e; j++){
-            new_arr.push(parseInt(grid[j].innerText, 10));
+            const val = parseInt(grid[j].innerText, 10);
+            if(isNaN(val))
+                new_arr.push(0);
+            else
+                new_arr.push(val);
         }
         let filtered_arr = cb(new_arr);
         let v = 0;
         for(let k = s; k < e; k++){
-            grid[k].innerText = filtered_arr[v];
+            if(filtered_arr[v] == 0){
+                grid[k].innerText = " ";
+            } else{
+                grid[k].innerText = filtered_arr[v];
+            }
             v++;
         }
         s += 4;
         e += 4;
     }
 }
+
 function coloumnOperation(cb){
     const grid = Array.from(document.querySelector('.grid').children);
     for(let i = 0; i < 4; i++){
         let new_arr = []
         for(let j = i; j < 16; j+=4){
-            new_arr.push(parseInt(grid[j].innerText, 10));
+            const val = parseInt(grid[j].innerText, 10);
+            if(isNaN(val))
+                new_arr.push(0);
+            else
+                new_arr.push(val);
         }
         let filtered_arr = cb(new_arr);
         let v = 0;
         for(let j = i; j < 16; j+=4){
-            grid[j].innerText = filtered_arr[v];
+            if(filtered_arr[v] == 0){
+                grid[j].innerText = " ";
+            } else{
+                grid[j].innerText = filtered_arr[v];
+            }
             v++;
         }
     }
@@ -69,9 +82,6 @@ function ShiftingTiles(direction){
     if(direction == 'R') rowOperation(rightshift);
     if(direction == 'U') coloumnOperation(leftshift);
     if(direction == 'D') coloumnOperation(rightshift);
-}
-function scoreUpdate(){
-    
 }
 function mergeOperationForRowL(arr){ 
     for(let i = 0; i < 3; i++){
@@ -101,33 +111,33 @@ function mergeOperationForRowR(arr){
     }
     return arr;
 }
+function colorGrading(){
+    const grid = Array.from(document.querySelector('.grid').children);
+    grid.forEach(it => {
+
+    });
+}
 function isGameOver(){
     const grid = Array.from(document.querySelector('.grid').children);
-    let filtered_grid = [];
-    filtered_grid = grid.filter(it => it.innerText == 0);
-    if(filtered_grid.length == 0){
-        let s = 0, e = 4;
-        for(let i = 0; i < 4; i++){
-            for(let j = s; j < e-1; j++){
-                if(grid[j].innerText === grid[j+1].innerText){
-                    return false;
-                } 
-            }
-            s += 4;
-            e += 4;
+    let filtered_grid = grid.filter(it => it.innerHTML == " ");
+    // console.log(filtered_grid);
+    if(filtered_grid.length != 0) return false;
+    let s = 0, e = 4;
+    for(let i = 0; i < 4; i++){
+        for(let j = s; j < e-1; j++){
+            if(grid[j].innerText === grid[j+1].innerText)
+                return false;
         }
-        for(let i = 0; i < 4; i++){
-            for(let j = i; j < 12; j+=4){
-                if(grid[j].innerText === grid[j+4].innerText){
-                    return false;
-                } 
-            }
+        s += 4;
+        e += 4;
+    }
+    for(let i = 0; i < 4; i++){
+        for(let j = i; j < 12; j+=4){
+            if(grid[j].innerText === grid[j+4].innerText)
+                return false;
         }
-        return true;
     }
-    else{
-        return false;
-    }
+    return true;
 }
 function checkForWin(){
     const grid = Array.from(document.querySelector('.grid').children);
@@ -153,41 +163,46 @@ function restartGame(){
     generate();
     document.querySelector("body").addEventListener("keyup", control);
 }
-function blockZeros(){
-    const grid = Array.from(document.querySelector(".grid").children);
-    grid.forEach((it) =>{
-        if(it.innerText == 0)
-            it.innerText.style.visibility = "hidden";
-        else
-            it.innerText.style.visibility = "visible";
-    })
+function check(before){
+    const after = document.querySelector('.grid').children;
+    for(let i = 0; i < before.length; i++){
+        if(before[i].innerHTML != after[i].innerHTML) return true;
+    }
+    return false;
 }
-
 const control = (e)=>{
     let prev_score = parseInt(document.querySelector('#score').innerText, 10);
     if(e.keyCode === 37){
+        const grid = (document.querySelector('.grid')).cloneNode(true);
         ShiftingTiles('L');
         rowOperation(mergeOperationForRowL);
         ShiftingTiles('L');
-        generate();
+        if(check(grid.children))
+            generate();
     } 
     else if(e.keyCode === 40){
+        const grid = (document.querySelector('.grid')).cloneNode(true);
         ShiftingTiles('D');
         coloumnOperation(mergeOperationForRowR);
         ShiftingTiles('D');
-        generate(); 
+        if(check(grid.children))
+            generate();
     } 
     else if(e.keyCode === 38){
+        const grid = (document.querySelector('.grid')).cloneNode(true);
         ShiftingTiles('U');
         coloumnOperation(mergeOperationForRowL);
         ShiftingTiles('U');
-        generate();  
+        if(check(grid.children))
+            generate();
     } 
     else if(e.keyCode === 39){
+        const grid = (document.querySelector('.grid')).cloneNode(true);
         ShiftingTiles('R');
         rowOperation(mergeOperationForRowR);
         ShiftingTiles('R');
-        generate();  
+        if(check(grid.children))
+            generate();
     }
     let curr_score = parseInt(document.querySelector('#score').innerText, 10);
     if(curr_score > prev_score)
@@ -197,7 +212,6 @@ const control = (e)=>{
         document.querySelector('#result').style.color = "red";
         document.querySelector("body").removeEventListener("keyup", control);
     }
-    // blockZeros();
 }
 createBoard();
 generate();
